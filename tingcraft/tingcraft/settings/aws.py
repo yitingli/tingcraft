469ed682ec9c71be146acffd19f76ac638b455c4
Yiting Li <yl2919@columbia.edu>
2013-08-06 15:45:33 -0400

    db change in aws settings

--- tingcraft/tingcraft/settings/aws.py
+++ tingcraft/tingcraft/settings/aws.py
@@ -11,7 +11,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'yitingvvv',
-        'HOST': 'mysql.cvqkihah2kll.ap-southeast-1.rds.amazonaws.com',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'dbinstance.cvqkihah2kll.ap-southeast-1.rds.amazonaws.com',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
