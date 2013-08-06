4fcac9bc41008d31758f4be3ae4bd59cf596de7e
Yiting Li <yl2919@columbia.edu>
2013-08-06 11:40:04 -0400

    db change

--- tingcraft/tingcraft/settings/aws.py
+++ tingcraft/tingcraft/settings/aws.py
@@ -3,6 +3,20 @@ from base import *
 DEBUG = False
 ALLOWED_HOSTS = ['.liyiting.net']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # To create one in MySQL: CREATE DATABASE tingcraft CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+        'NAME': 'sitedb',                    # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': 'yitingvvv',
+        'HOST': 'mysql.cvqkihah2kll.ap-southeast-1.rds.amazonaws.com',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
+    }
+}
+
+
 AWS_ACCESS_KEY_ID = 'AKIAIS4JPQBNHQAHBWIA'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_S3_SECURE_URLS = False
@@ -19,18 +33,5 @@ DEFAULT_AVATAR_LOCATION = STATIC_URL + 'img/avatar/'
 
 STATICFILES_STORAGE = 'core.s3storage.S3PipelineStorage'
 
-IMAGE_PATH_PREFIX = 'media/images/origin'
 MEDIA_URL = 'http://%s/' % (AWS_S3_CUSTOM_DOMAIN)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        # To create one in MySQL: CREATE DATABASE tingcraft CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-        'NAME': 'mysite',                    # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'yitingvvv',
-        'HOST': 'mysql.cvqkihah2kll.ap-southeast-1.rds.amazonaws.com',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
-    }
-}
+IMAGE_PATH_PREFIX = 'media/images/origin'
