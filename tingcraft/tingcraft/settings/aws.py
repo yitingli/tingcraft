153a2e36fa12ca9d8eae4eb595bd9482355a7a6d
Yiting Li <yl2919@columbia.edu>
2013-08-07 00:53:42 -0400

    turn on debug mode

--- tingcraft/tingcraft/settings/aws.py
+++ tingcraft/tingcraft/settings/aws.py
@@ -1,7 +1,9 @@
 from base import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['.liyiting.net']
+
+THUMBNAIL_DEBUG = True
 
 DATABASES = {
     'default': {
@@ -35,3 +37,32 @@ STATICFILES_STORAGE = 'core.s3storage.S3PipelineStorage'
 
 MEDIA_URL = 'http://%s/' % (AWS_S3_CUSTOM_DOMAIN)
 IMAGE_PATH_PREFIX = 'media/images/origin'
+
+if DEBUG:
+    INTERNAL_IPS = ('127.0.0.1', '66.65.162.33')
+
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        #'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.cache.CacheDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
