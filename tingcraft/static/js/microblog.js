5b1707e19e7e04c6f22ccbfad49575ba06ae5c1e
Yiting Li <yl2919@columbia.edu>
2013-09-28 01:33:33 -0400

    added checking of reaching the end at microblog and loading effect

--- tingcraft/static/js/microblog.js
+++ tingcraft/static/js/microblog.js
@@ -3,11 +3,25 @@
         var user_id = $('#header-profile').attr('data-id');
         var max_id = $('.microblog-container:last').attr('data-id');
         var url_base = window.location.href.split('?')[0];
+        var t = $(this)
+        t.removeClass('btn-primary');
+        t.html('Loading...');
         $.ajax({
             type: 'GET',
             url: url_base + '?max_id=' + max_id,
             success: function(data) {
-                $('#microblog-list-container').append(data);
+                if ($(data).filter('div').length) {
+                    setTimeout(function() {
+                        $('#microblog-list-container').append(data);
+                        t.addClass('btn-primary');
+                        t.html('Load More...');
+                    }, 500);
+                } else {
+                    setTimeout(function() {
+                        $('#microblog-list-container').append(data);
+                        t.html('We have reached the end...');
+                    }, 500);
+                }
             },
             error: function(res) {
             }
