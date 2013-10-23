ad6d19961f01de0d34193c80b4d8d3b1e9121661
Yiting Li <yl2919@columbia.edu>
2013-10-23 01:13:06 -0400

    code in ckeditor

--- tingcraft/static/plugin/ckeditor/config.js
+++ tingcraft/static/plugin/ckeditor/config.js
@@ -7,4 +7,14 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.toolbarGroups = [
+        { name: 'document', groups: [ 'mode'] }, // shows the source button
+        { name: 'pbckcode' } ,                   // shows the pbckcode button
+    ];
+    config.extraPlugins = 'pbckcode';
+    config.pbckcode = {
+        modes :  [ ['C++', 'c_pp'], ['Python', 'py'] ],
+        theme : 'clouds',
+        highlighter : "PRETTIFY"
+    };
 };
